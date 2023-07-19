604cc0ed22d876196e1a98767c0209da081f3cad
Magui Martin <Magali Martin>
2023-07-19 16:41:54 -0300

    Route Landing-NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css'
 import Landing from './views/Landing'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import React from 'react'
+import NavBar from './components/NavBar'
+import Home from './views/Home'
 
 function App() {
-
+  const location = useLocation()
   return (
     <div>
+      {location.pathname !== "/" && <NavBar/>}
       <Routes>
+        <Route exact path = "/" element={<Landing/>}/>
+        <Route path="/home" element={<Home/>}/>
       </Routes>
     </div>
   )
