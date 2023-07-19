c37a2fa8afa67a11d14d51f69752f508a4fb4fd3
FSilvaFlores <franco2208silva@gmail.com>
2023-07-19 01:15:20 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import Landing from './views/Landing'
+// import Landing from './views/Landing'
+import Home from './views/Home/Home'
 import { Route, Routes } from 'react-router-dom'
 import React from 'react'
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
       <Routes>
+        <Route path='/home' element={<Home/>}/>
       </Routes>
     </div>
   )
