cebdef16f8cd10d8fef50ce5b915e921bbc1156d
FSilvaFlores <franco2208silva@gmail.com>
2023-07-21 02:24:07 -0500

    filter update

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -13,17 +13,13 @@ import { getAllBooks } from '../../redux/actions';
 const Home = () => {
 
 const dispatch = useDispatch();
-const books = useSelector((state) => state.AllBooks);
-console.log(books);
+
+const copyState = useSelector((state) => state.copyState);
+console.log(copyState);
 
 const [page, setPage] = useState(1);
 const [perPage, setPerPage] = useState(6);
-
-const idxLast = page * perPage;
-const idxFirst = idxLast - perPage;
-const currentData = books.slice(idxFirst, idxLast);
-const max = Math.ceil(books.length / perPage);
-
+const [filteredCopy, setFilteredCopy] = useState(copyState);
 
 useEffect(() => {
   dispatch(getAllBooks());
@@ -31,25 +27,52 @@ useEffect(() => {
 }, [dispatch]);
 
 
-  return (
-    <div className={style.booksContainer}> 
-      <header className={style.searchContainer}>
-        <SearchBar/>
-      </header>
-      <aside className={style.FilterContainer}>
-        <Filters/>
-      </aside>
-      <div className={style.cardContainer}>
-        {(currentData?.map((book,idx)=>(
-          <div key={idx} className={style.card}>
-            <Card book={book}/>
-          </div>
-        ))
-        )}
-      </div>
-      <Pagination page={page} setPage={setPage} perPage={perPage} max={max}/>
-    </div>
-  );
+const updateFilterSelect = (filteredData) => {
+  setFilteredCopy(filteredData);
 };
 
+const idxLast = page * perPage;
+const idxFirst = idxLast - perPage;
+const currentData = copyState?.slice(idxFirst, idxLast);
+const max = Math.ceil(copyState?.length / perPage);
+
+
+return (
+  <div className={style.booksContainer}>
+    <header className={style.searchContainer}>
+      <SearchBar/>
+    </header>
+    <aside className={style.FilterContainer}>
+    <Filters copyState ={copyState} updateFilterSelect={updateFilterSelect} setPage={setPage}/>
+    </aside>
+    <div className={style.cardContainer}>
+      <div className={style.row}>
+        {currentData?.slice(0, 3).map((book, idx) => (
+          <div key={idx} className={style.card}>
+            <Card book={book} />
+          </div>
+        ))}
+      </div>
+      <div className={style.row}>
+        {currentData?.slice(3, 6).map((book, idx) => (
+          <div key={idx} className={style.card}>
+            <Card book={book} />
+          </div>
+        ))}
+      </div>
+    </div>
+    <Pagination page={page} setPage={setPage} perPage={perPage} max={max} />
+  </div>
+);
+
+};
+
+    
 export default Home;
+
+
+
+
+
+
+
