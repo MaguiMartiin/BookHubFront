72dd369e3c4db611127553ac432fc6fa7627f5a0
yonatanlla <yonatanllanto174@gmail.com>
2023-07-21 15:28:29 -0500

    form

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-<<<<<<<<< Temporary merge branch 1
 import './App.css'
 // import Landing from './views/Landing'
 import Home from './views/Home/Home'
 import { Route, Routes } from 'react-router-dom'
-=========
 import Landing from './views/Landing'
 import { Route, Routes, useLocation } from 'react-router-dom'
->>>>>>>>> Temporary merge branch 2
 import React from 'react'
 import NavBar from './components/NavBar'
 import Home from './views/Home'
+import Form from "./views/Form/FormCreate"
 
 function App() {
   const location = useLocation()
@@ -17,12 +15,9 @@ function App() {
     <div>
       {location.pathname !== "/" && <NavBar/>}
       <Routes>
-<<<<<<<<< Temporary merge branch 1
         <Route path='/home' element={<Home/>}/>
-=========
         <Route exact path = "/" element={<Landing/>}/>
-        <Route path="/home" element={<Home/>}/>
->>>>>>>>> Temporary merge branch 2
+        <Route path="/form" element={<Form/>}/>
       </Routes>
     </div>
   )
