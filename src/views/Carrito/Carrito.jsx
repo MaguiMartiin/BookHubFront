a89f621591bed3cdfed6d2cc564e9befaf9295bf
yonatanlla <yonatanllanto174@gmail.com>
2023-07-29 17:58:48 -0500

    carrito_cambios

--- src/views/Carrito/Carrito.jsx
+++ src/views/Carrito/Carrito.jsx
@@ -1,128 +1,146 @@
-import style from './Carrito.module.css'
-import React from 'react';
-import { useEffect, useState } from 'react';
+import style from "./Carrito.module.css";
+import React from "react";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { deleteFromCart } from '../../redux/actions';
-import Swal from 'sweetalert2';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { deleteFromCart } from "../../redux/actions";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Carrito = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const cart = useSelector((state) => state.cart);
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const cart = useSelector((state) => state.cart);
 
- 
-  const [totalPrice, setTotalPrice] = useState(0);
-  const [selectedQuantities, setSelectedQuantities] = useState({});
+	const [totalPrice, setTotalPrice] = useState(0);
+	console.log(totalPrice);
+	const [selectedQuantities, setSelectedQuantities] = useState({});
 
+	useEffect(() => {
+		if (totalPrice === 0) {
+			const timer = setTimeout(() => {
+				Swal.fire({
+					title: "The cart is empty",
+					icon: "warning",
+				}).then(() => {
+					navigate("/home");
+				});
+			}, 300);
+			return () => clearTimeout(timer);
+		}
+	}, [totalPrice, navigate]);
 
-  useEffect(() => {
-    if (totalPrice === 0) {
-      const timer = setTimeout(() => {
-        Swal.fire({
-          title: 'The cart is empty',
-          icon: 'warning',
-        }).then(() => {
-          navigate('/home');
-        });
-      }, 300);
-      return () => clearTimeout(timer);
-    }
-  }, [totalPrice, navigate]);
+	useEffect(() => {
+		const calculateTotalPrice = () => {
+			let total = 0;
+			cart?.forEach((book) => {
+				total += book.price * selectedQuantities[book.id];
+			});
+			setTotalPrice(total);
+		};
+		calculateTotalPrice();
+	}, [cart, selectedQuantities]);
 
-  useEffect(() => {
-    const calculateTotalPrice = () => {
-      let total = 0;
-      cart?.forEach((book) => {
-        total += book.price * selectedQuantities[book.id];
-      });
-      setTotalPrice(total);
-    };
-    calculateTotalPrice();
-  }, [cart, selectedQuantities]);
+	useEffect(() => {
+		const initialQuantities = cart?.reduce((quantities, book) => {
+			quantities[book.id] = 1;
+			return quantities;
+		}, {});
+		setSelectedQuantities(initialQuantities);
+	}, [cart]);
 
-  useEffect(() => {
-    const initialQuantities = cart?.reduce((quantities, book) => {
-      quantities[book.id] = 1;
-      return quantities;
-    }, {});
-    setSelectedQuantities(initialQuantities);
-  }, [cart]);
+	const handleQuantityChange = (bookId, value) => {
+		setSelectedQuantities((prevQuantities) => ({
+			...prevQuantities,
+			[bookId]: Math.max(1, prevQuantities[bookId] + value),
+		}));
+	};
 
+	const handleDeleteItem = (itemId) => {
+		dispatch(deleteFromCart(itemId));
+		const updatedCart = cart.filter((item) => item.id !== itemId);
+		localStorage.setItem("cart", JSON.stringify(updatedCart));
+	};
 
-  const handleQuantityChange = (bookId, value) => {
-    setSelectedQuantities((prevQuantities) => ({
-      ...prevQuantities,
-      [bookId]: Math.max(1, prevQuantities[bookId] + value), 
-    }));
-  };
+	const itemsMapped = cart.map((item) => ({
+		book_id: item.id,
+		title: item.name,
+		quantity: selectedQuantities[item.id],
+		unit_price: item.price,
+	}));
 
-  const handleDeleteItem = (itemId) => {
-    dispatch(deleteFromCart(itemId))
-    const updatedCart = cart.filter((item) => item.id !== itemId);
-    localStorage.setItem('cart', JSON.stringify(updatedCart));
-  }
+	console.log(itemsMapped, "itemsMapped");
 
-  const itemsMapped = cart.map((item) => ({
-    item_id: item.id,
-    title: item.name,
-    quantity: cart.length, 
-    totalAmount: totalPrice,
-  }));
-  
+	const handleClick = () => {
+		axios
+			.post("http://localhost:3001/payment", {
+				products: itemsMapped,
+				totalPrice: totalPrice,
+				title: "Compra de libros",
+			})
+			.then((response) => {
+				window.location.href = response.data.url;
+			});
+	};
 
-   const handleClick = () => {
-    axios.post('/payment',itemsMapped)
-    .then((response) => {
-      return response.data
-    })
-    .then((data) => {
-      window.location.href = data.init_point
-    })
-  }
-
-
-  return (
-    <div className={style.cartContainer}>
-      <div className={style.cardContainer}>
-        {cart?.map((book) => (
-          <div key={book.id} className={style.card}>
-            <div className={style.imageContainer}>
-              <img src={book.image} alt={book.name} className={style.bookImage} />
-            </div>
-            <div className={style.bookInfo}>
-              <h1>{book.name}</h1>
-              <h2>Precio: ${book.price}</h2>
-              <p>Disponibles: {book.available}</p>
-              <p>Género: {book.Gender?.name}</p>
-              <p>Autor: {book.Author?.name}</p>
-              <div className={style.deleteButtonContainer}>
-              <button className={style.deleteButton} onClick={() => handleDeleteItem(book.id)}>X</button>
-              </div>
-              <div className={style.quantityControls}>
-                <button className={style.quantityButton} onClick={() => handleQuantityChange(book.id, -1)}>-</button>
-                <input
-                  type="number"
-                  value={selectedQuantities[book.id]}
-                  min="1"
-                  readOnly
-                  className={style.quantityInput}
-                />
-                <button className={style.quantityButton} onClick={() => handleQuantityChange(book.id, 1)}>+</button>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-      <div className={style.totalPriceContainer}>
-        <h3>Total:</h3>
-        <p className={style.totalPrice}>${totalPrice}</p>
-        <button className={style.payButton} onClick={handleClick} > PAY! </button>
-      </div>
-    </div>
-  );
-}
+	return (
+		<div className={style.cartContainer}>
+			<div className={style.cardContainer}>
+				{cart?.map((book) => (
+					<div key={book.id} className={style.card}>
+						<div className={style.imageContainer}>
+							<img
+								src={book.image}
+								alt={book.name}
+								className={style.bookImage}
+							/>
+						</div>
+						<div className={style.bookInfo}>
+							<h1>{book.name}</h1>
+							<h2>Precio: ${book.price}</h2>
+							<p>Disponibles: {book.available}</p>
+							<p>Género: {book.Gender?.name}</p>
+							<p>Autor: {book.Author?.name}</p>
+							<div className={style.deleteButtonContainer}>
+								<button
+									className={style.deleteButton}
+									onClick={() => handleDeleteItem(book.id)}>
+									X
+								</button>
+							</div>
+							<div className={style.quantityControls}>
+								<button
+									className={style.quantityButton}
+									onClick={() => handleQuantityChange(book.id, -1)}>
+									-
+								</button>
+								<input
+									type="number"
+									value={selectedQuantities[book.id]}
+									min="1"
+									readOnly
+									className={style.quantityInput}
+								/>
+								<button
+									className={style.quantityButton}
+									onClick={() => handleQuantityChange(book.id, 1)}>
+									+
+								</button>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+			<div className={style.totalPriceContainer}>
+				<h3>Total:</h3>
+				<p className={style.totalPrice}>${totalPrice}</p>
+				<button className={style.payButton} onClick={handleClick}>
+					{" "}
+					PAY!{" "}
+				</button>
+			</div>
+		</div>
+	);
+};
 
 export default Carrito;
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,28 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
+		fontFamily: {
+			primary: ["Baloo", "sans-serif"],
+			secondary: ["Abel", "serif"],
+			tertiary: ["Alegreya Sans"],
+		},
+		container: {
+			padding: {
+				DEFAULT: "15px",
+			},
+		},
+		screens: {
+			sm: "480px",
+			md: "768px",
+			lg: "976px",
+			xl: "1440px",
+		},
+
 		extend: {
 			colors: {
-				primary: "#FF988B",
-				primaryLight: "#FAFAFA",
-				secondaryBorderLight: "#E5E5E5",
-				customColor0: "#ff90c0",
-				customColor1: "#ff65a9",
-				customColor2: "#c3418e",
-				customColor3: "#793474",
-				customColor4: "#44295f",
+				primary: "#ff988b",
+				secondary: "ffd8d4",
+				tertiary: "ffbfb7",
 			},
 		},
 	},
