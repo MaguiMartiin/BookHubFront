9b5d426d1e45c8ca888a3d5676478d050e4b4a43
yonatanlla <yonatanllanto174@gmail.com>
2023-07-28 23:23:04 -0500

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,39 @@
-import './App.css'
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { refreshCart } from './redux/actions'
-import { Route, Routes, useLocation } from 'react-router-dom'
-import { useEffect } from 'react'
-import Landing from './views/Landing/Landing'
-import Home from './views/Home/Home'
-import NavBar from './components/NavBar/NavBar'
-import Detail from './views/Detail/Detail'
+import "./App.css";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { refreshCart } from "./redux/actions";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import Landing from "./views/Landing/Landing";
+import Home from "./views/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
+import Detail from "./views/Detail/Detail";
 //import Landing from './views/Landing'
-import Form from "./views/Form/FormCreate"
+import Form from "./views/Form/FormCreate";
 // user
-import Login from './views/User/Login'
-import EditDetail from './views/Detail/EditDetail'
-import SignUp from './views/User/SignUp'
-import Carrito from './views/Carrito/Carrito'
-import axios from 'axios';
+import Login from "./views/User/Login";
+import EditDetail from "./views/Detail/EditDetail";
+import SignUp from "./views/User/SignUp";
+import Carrito from "./views/Carrito/Carrito";
+import axios from "axios";
+import MyBooks from "./views/MyBooks/MyBooks";
 
-axios.defaults.baseURL = "https://servidor-libreria.onrender.com"
-
-import MyBooks from './views/MyBooks/MyBooks'
+axios.defaults.baseURL = "https://servidor-libreria.onrender.com";
 
 
 function App() {
-   const location = useLocation();
-   const dispatch = useDispatch();
+	const location = useLocation();
+	const dispatch = useDispatch();
 
-   useEffect(() => {
-    const carrito = localStorage.getItem('cart');
-    const carritoRefresh = JSON.parse(carrito);
-    if (carritoRefresh ) {
-      dispatch(refreshCart(carritoRefresh));
-    }
-  }, [dispatch]);
+	useEffect(() => {
+		const carrito = localStorage.getItem("cart");
+		const carritoRefresh = JSON.parse(carrito);
+		if (carritoRefresh) {
+			dispatch(refreshCart(carritoRefresh));
+		}
+	}, [dispatch]);
 
-  return (
+	return (
 		<div>
 			{location.pathname !== "/" &&
 				location.pathname !== "/login" &&
@@ -47,8 +46,8 @@ function App() {
 				<Route path="/form" element={<Form />} />
 				<Route path="/login" element={<Login />}></Route>
 				<Route path="/signup" element={<SignUp />}></Route>
-				<Route path="/MyBooks" element={<MyBooks/>}/>
-			   <Route path="/carrito" element={<Carrito/>}/>
+				<Route path="/MyBooks" element={<MyBooks />} />
+				<Route path="/carrito" element={<Carrito />} />
 			</Routes>
 		</div>
 	);
