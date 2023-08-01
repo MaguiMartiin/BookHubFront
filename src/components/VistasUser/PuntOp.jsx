3e21ebe02c77beece692465e91b529948fb8c996
Magui Martin <Magali Martin>
2023-08-01 14:55:45 -0300

    Punt y Op func.

--- src/components/VistasUser/PuntOp.jsx
+++ src/components/VistasUser/PuntOp.jsx
@@ -1,6 +1,18 @@
 import React from "react"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getOpinion, getPuntuation } from "../../redux/actions"
 
 const Opiniones = () => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getPuntuation())
+        dispatch(getOpinion())
+    }, [dispatch])
+    const misPuntuaciones = useSelector(state => state.puntuations)
+    const misOpiniones = useSelector(state => state.opinion)
+    console.log();
+
     return (
         <div>
             <h1>Opiniones</h1>
