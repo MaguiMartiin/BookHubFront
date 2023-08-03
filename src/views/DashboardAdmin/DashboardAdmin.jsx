d8c23e73c1493c189a3e176791f13bc276f05210
GaboYopasa <gyopasaa@gmail.com>
2023-08-02 22:05:36 -0500

    working

--- src/views/DashboardAdmin/DashboardAdmin.jsx
+++ src/views/DashboardAdmin/DashboardAdmin.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import SearchBar from "../../components/SearchBar/SearchBar";
-import Card from "../../components/Card/Card";
+import Card from "../../components/DashBoard Components/Card";
 import Pagination from "../../components/Pagination/Pagination";
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllBooks, addToCart, refreshCart } from "../../redux/actions";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import style from "./DashboardAdmin.module.css"
 
@@ -16,6 +17,8 @@ const DashboardAdmin = () => {
 
     const [page, setPage] = useState(1);
     const [perPage, setPerPage] = useState(6);
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         const compra_id = localStorage.getItem("compra_id");
@@ -48,7 +51,7 @@ const DashboardAdmin = () => {
                 <SearchBar setPage={setPage} />
             </header>
 
-            <div className="max-w-screen-2xl h-[670px] px-40 mt-4 xl:h-[690]">
+            <div className="max-w-screen-2xl h-[350px] px-40 mt-4 xl:h-[690]">
                 <div className="grid grid-flow-row gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mb-4 uppercase  ">
                     {currentData?.slice(0, 3).map((book, idx) => (
                         <div key={idx} className={style.card}>
@@ -59,6 +62,22 @@ const DashboardAdmin = () => {
             </div>
             <div className="flex justify-center">
                 <Pagination page={page} setPage={setPage} perPage={perPage} max={max} />
+            </div>
+            <div className="flex flex-col items-center mt-10">
+                <button
+                    className="py-3 px-6 bg-red-500 text-white rounded-md shadow-md hover:bg-red-600 mb-4"
+                    onClick={() => { navigate("/form") }}
+                >
+                    Realizar una publicación
+                </button>
+            </div>
+            <div className="flex flex-col items-center mt-10">
+                <button
+                    className="py-3 px-6 bg-red-500 text-white rounded-md shadow-md hover:bg-red-600 mb-4"
+                    onClick={() => { navigate("/editUsers") }}
+                >
+                    Editar Usuarios
+                </button>
             </div>
         </div>
     );
