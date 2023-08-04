3eb90f8562356eb57940e965e0592113d0ae567e
Dionel22 <diondiaz293@gmail.com>
2023-08-04 20:48:52 -0300

    axios

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 
-axios.defaults.baseURL = "http://localhost:3001" //"https://servidor-libreria.onrender.com/";
+axios.defaults.baseURL = "https://servidor-libreria.onrender.com/";
 
 
 function App() {
